da8622987598e388d2c389b36958479b0a406b49
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2024-02-19 19:10:37 -0300

    Course, Module and Lessoan endpoints created!

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,7 +1,9 @@
 package com.ead.course.repositories;
 
+import com.ead.course.models.CourseModel;
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,7 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.List;
 
-public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID>, JpaSpecificationExecutor<ModuleModel> {
     /*
     @EntityGraph(attributePaths = {"course"})
     ModuleModel findByTitle(String title);
